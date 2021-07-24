05867e5225ae5d338c8e9503a8c6ef7b78be22f4
mosharafkarim <69795428+mosharafkarim@users.noreply.github.com>
2021-07-24 18:33:09 +0530

    Workshop22

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Login from './Components/Login'
+import Logout from './Components/Logout'
+import { selectUser } from './features/userSlice'
+
+
+const App = () => {
+  const user=useSelector(selectUser)
+  return (
+    <div>
+      {user ? <Logout/> : <Login/>}
+    </div>
+  )
+}
+
+export default App
